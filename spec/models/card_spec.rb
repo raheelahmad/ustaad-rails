25eade9ff24214ca75a85ab1df464e66c2907543
Raheel Ahmad <rahmad@fitbit.com>
2013-01-05 23:23:35 -0800

    Test for public in Card

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
 describe Card do
+  before(:each) { @card = Card.new(question:"A", answer:"B") }
+  subject { @card }
   it "should not save without a question and answer" do
-    @card = Card.new
+    @card.answer = nil
+    @card.should_not be_valid
+
+    @card.answer = "something"
+    @card.question = nil
     @card.should_not be_valid
   end
 
+  it "should have public flag as false when being created" do
+    @card.save
+    @card.public.should be_false
+  end
 end
